a685a37094299e4c6235e64cce3107a79fefeda4
Tony McCoy <tony@ratwizard.io>
2017-11-02 08:28:04 -0700

    Added buble loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
-var path = require("path");
+const path = require("path");
 
 module.exports = {
-  context: path.join(__dirname, "src"),
-  entry: "./orbit.js",
-  output: {
-    path: path.join(__dirname, "build"),
-    filename: 'bundle.js'
-  },
-  // devtool: "inline-source-map"
+	context: path.join(__dirname, "src"),
+	entry: "./orbit.js",
+	output: {
+		path: path.join(__dirname, "build"),
+		filename: "bundle.js"
+	},
+	loaders: [{
+		test: /.js$/,
+		loaders: "buble",
+		include: path.join(__dirname, "src"),
+		query: {
+			objectAssign: "Object.assign",
+		}
+	}],
 };
