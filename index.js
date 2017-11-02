cc1538df8bd5b3c86ba353819a7ceaf8912d33c0
Tony McCoy <tony@ratwizard.io>
2017-11-02 08:30:39 -0700

    Remove dokku deploy stuff

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3035;
-
-app.use(express.static('build'));
-
-app.listen(PORT, () => {
-	console.log(`Orbit is running on port ${PORT}`);
-});
